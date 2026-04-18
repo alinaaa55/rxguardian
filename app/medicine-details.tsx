30b00dbf6ae30f142bc0e2e5b1d81b6f3de19873
Alina Shaikh <alinashaikh@Alinas-MacBook-Air.local>
2026-04-18 11:22:32 +0530

    updated meds screen

--- app/medicine-details.tsx
+++ app/medicine-details.tsx
@@ -1,13 +1,12 @@
 import { Feather, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import React from "react";
 import {
-    SafeAreaView,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
 
 export default function MedicineDetails() {
